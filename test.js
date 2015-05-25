4430285697d8065c086e5e7b7afe40fcb7ba0d95
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-25 13:33:06 +0200

    minor tweaks

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ var isArchive = require('./');
 test(function (t) {
 	t.assert(isArchive('unicorn.zip'));
 	t.assert(isArchive('unicorn.tar'));
+	t.assert(isArchive('unicorn.TAR'));
 	t.assert(!isArchive('unicorn.jpg'));
 	t.assert(!isArchive('unicornzip'));
 	t.assert(!isArchive('unicorn.txt'));
